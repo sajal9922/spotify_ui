938bfe6e110b07a8ae2a6080c77c4cae379b37c7
Sajal <sajal.moni@gmail.com>
2024-09-30 15:58:09 +0300

    test

--- src/components/spotifyUi/SpotifyUi.jsx
+++ src/components/spotifyUi/SpotifyUi.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const SpotifyUi = () => {
-  return <div>Genetating front end code</div>;
+  return (
+    <>
+      <h1>Spotify UI</h1>
+      <div>Genetating front end code</div>
+    </>
+  );
 };
 
 export default SpotifyUi;
